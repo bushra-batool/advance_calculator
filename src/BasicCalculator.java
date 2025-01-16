ec0103f0c378da5858b92bcfe4f62124fdfa497c
shumailamustafa <mshumaila360@gmail.com>
2025-01-16 15:36:50 +0500

    Make Switch condition

--- src/BasicCalculator.java
+++ src/BasicCalculator.java
@@ -28,5 +28,50 @@ public class BasicCalculator {
         if (operator == ' ') {
             System.out.println("Invalid Operator: valid operators are (+)(-)(*)(/)");
         }
+         //now splitting the string to get operands for operation by using split() method form split class
+        String[] operands = expression.split("[" + operator + "]");
+        double num_1 = Double.parseDouble(operands[0]);
+        double num_2 = Double.parseDouble(operands[1]);
+
+        //now performing the calculations based on the operations
+        switch (operator) {
+            // If the operator is addition
+            case '+' -> {
+                // Perform addition using the Operators class
+                double addition = operators.addition((int) num_1, (int) num_2);
+                // Print the result of the addition operation
+                System.out.printf("Result %d %s %d = %d", (int) num_1, operator, (int) num_2,(int) addition);
+            }
+            // If the operator is subtraction
+            case '-' -> {
+                // Perform subtraction using the Operators class
+                double subtraction = operators.subtraction((int) num_1, (int) num_2);
+                // Print the result of the subtraction operation
+                System.out.printf("Result %d %s %d = %d", (int) num_1, operator, (int) num_2,(int) subtraction);
+            }
+            // If the operator is multiplication
+            case '*' -> {
+                // Perform multiplication using the Operators class
+                double multiplication = operators.multiplication((int) num_1, (int) num_2);
+                // Print the result of the multiplication operation
+                System.out.printf("Result %d %s %d = %d", (int) num_1, operator, (int) num_2,(int) multiplication);
+            }
+            // If the operator is division
+            case '/' -> {
+                // Check for division by zero
+                if (num_1 == 0 || num_2 == 0) {
+                    // Display an error message for division by zero
+                    System.out.println("Error: Cannot divide by 0");
+                    break;
+                }
+                // Perform division using the Operators class
+                double division = operators.division(num_1, num_2);
+                // Print the result of the division operation
+                System.out.println("Result: " + num_1 + " " + operator + " " + num_2 + " = " + division);
+            }
+            // If the entered operator is not valid
+       
+        }
+    }
     }
 }
