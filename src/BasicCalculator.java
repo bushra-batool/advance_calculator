c837bfb66d08189c1a7cbbe7712c1f0c1123d3d4
moazzam-samoo <moazzamsamoo819@gmail.com>
2025-01-16 15:24:33 +0500

    change class name

---
+++ src/BasicCalculator.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class BasicCalculator {
+    public static void main(String[] args) {
+
+        //creating Scanner object for taking user input
+        Scanner input = new Scanner(System.in);
+//        Operators operators = new Operators();
+
+    }
+}
